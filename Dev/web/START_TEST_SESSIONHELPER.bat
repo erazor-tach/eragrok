5282fc015327debe6940cf7ea563c33c75d9d9ba
erazor-tach <tachyon.fr@gmail.com>
2026-03-19 15:21:17 +0100

    Delete Dev directory

--- Dev/web/START_TEST_SESSIONHELPER.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo ========================================
-echo  TEST MODULE SessionHelper
-echo ========================================
-echo.
-echo Demarrage du serveur HTTP local...
-echo.
-cd /d E:\Projet\GIT\C\Dev\web
-python -m http.server 8000
